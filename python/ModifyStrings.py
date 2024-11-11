491052d9d3ad991835363c601ae420d3691ddb2e
shdrfq <shahidrafiq06@gmail.com>
2024-11-11 16:58:59 +0500

    add new file

--- python/ModifyStrings.py
+++ python/ModifyStrings.py
@@ -30,3 +30,18 @@ a = "Hello"
 b = "World"
 c = a + b
 print(c)
+
+"""
+F-Strings
+F-String was introduced in Python 3.6, and is now the preferred way of formatting strings.
+
+To specify a string as an f-string, simply put an f in front of the string literal, and add curly brackets {} as placeholders for variables and other operations.
+"""
+
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
