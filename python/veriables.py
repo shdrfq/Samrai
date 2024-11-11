dd4d4dbc23659beb2a4065477e41f018dcdb0ffd
shdrfq <shahidrafiq06@gmail.com>
2024-11-11 14:15:37 +0500

    file add variable_2

---
+++ python/veriables.py
@@ -0,0 +1,45 @@
+# creat variables
+
+x = 4
+x = "Sally"
+print(x)
+
+
+# Casting
+
+x = str(3)
+y = int(3)
+z = float(3)
+
+print(x)
+print(y)
+print(z)
+
+"""
+Get the Type
+You can get the data type of a variable with the type() function.
+
+"""
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+"""
+Single or Double Quotes?
+"""
+x = "John"
+print(x)
+#double quotes are the same as single quotes:
+x = 'John'
+print(x)
+
+"""
+Case-Sensitive
+"""
+
+a = 4
+A = "Sally"
+
+print(a)
+print(A)
