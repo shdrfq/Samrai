59765df3b7267cb14d878ccff512f25b319cd663
shdrfq <shahidrafiq06@gmail.com>
2024-11-11 13:08:49 +0500

    chang

--- python/helloworld.py
+++ python/helloworld.py
@@ -1,4 +1,17 @@
+# ctrl /  is shortcuto for comments
+#This is a comment
+#written in
+#more than just one line
+
 if 5 > 2:
  print("Five is greater than two!")  
 if 5 > 2:
         print("Five is greater than two!") 
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+
+print("Hello, World!")
