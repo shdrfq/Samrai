bcca199250240fc3a036715373eee03d0fe823d7
shdrfq <shahidrafiq06@gmail.com>
2024-11-12 16:46:04 +0500

    add file

--- python/pythonList.py
+++ python/pythonList.py
@@ -9,3 +9,24 @@ the other 3 are Tuple, Set, and Dictionary, all with different qualities and usa
 
 thislist = ["apple", "banana", "cherry"]
 print(thislist)
+
+
+#This will return the items from position 2 to 5.
+#Remember that the first item is position 0,
+#and note that the item in position 5 is NOT included
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+
+list1 = ["abc", 34, True, 40, "male"]
+print(list1)
+for x in list1:
+  print (x)
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5]) 
+
+
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
