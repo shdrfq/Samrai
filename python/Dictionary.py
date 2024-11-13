faf24ede74fc3d5a4ae64600e7af91fedff05192
shdrfq <shahidrafiq06@gmail.com>
2024-11-13 14:08:38 +0500

    add djang_work folder

--- python/Dictionary.py
+++ python/Dictionary.py
@@ -27,3 +27,103 @@ dictionary_2 = {
 }
 print(dictionary_2["brand"])
 print(dictionary_2["model"])
+
+print("===================")
+"""
+Loop Through a Dictionary
+"""
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict:
+  print(x)
+print("========value===========")
+"""
+Loop shahow value a Dictionary
+"""
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict.values():
+  print(x)
+
+print("======show keys=============")
+"""
+Loop Through a Dictionary
+"""
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict.keys():
+  print(x)
+
+print("======show value=============")
+"""
+Loop Through a Dictionary
+"""
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+for x in thisdict:
+  print(thisdict[x])
+
+print("======show item=============")
+"""
+Example
+Loop through both keys and values, by using the items() method:
+"""
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x, y in thisdict.items():
+  print(x, y)
+
+print("======COPY DICTIONALARY============")
+"""
+Example
+Loop through both keys and values, by using the items() method:
+"""
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x, y in thisdict.items():
+  print(x, y)
+
+
+# Copy a Dictionary
+# You cannot copy a dictionary simply by typing dict2 = dict1, because: dict2 will only be a reference to dict1, and changes made in dict1 will automatically also be made in dict2.
+# There are ways to make a copy, one way is to use the built-in Dictionary method copy().
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = thisdict.copy()
+print(mydict)
+for x in mydict.values():
+  print(x)  
+
+for x in mydict.items():
+  print(x)   
+
+# second way to make copy of existing available dictionary 
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = dict(thisdict)
+print(mydict)  
